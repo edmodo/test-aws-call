258e33e7f9bf62b986f4fe70c25209792c66f601
Merrick Luo <merrick@edmodo.com>
2020-07-15 11:33:53 +0800

    add test file upload script
    with some test file

---
+++ run.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'aws-sdk-v1'
+
+s3 = AWS::S3.new(
+  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+)
+
+bucket = s3.buckets['test-aws-call']
+if !bucket.exists?
+  s3.buckets.create('test-aws-call')
+end
+
+Pathname.new("./files").each_child do |file|
+  p "uploading #{file.to_s}"
+  bucket.objects[file.basename].write(file)
+end
